ae4aff486371e919e9a4f97302fa7822728f2d84
Thomas Lohse <thomas.k.lohse@gmail.com>
2024-12-01 21:04:14 +0100

    day one

---
+++ 1.hs
@@ -0,0 +1,31 @@
+import Data.List
+import Data.Map qualified as M
+import System.Environment
+import System.IO
+
+convert [] = ([], [])
+convert (x : xs) = (read a : as, read b : bs)
+  where
+    (as, bs) = convert xs
+    [a, b] = words x
+
+sumCached _ [] _ = 0
+sumCached m (a : as) l = case M.lookup a m of
+    Nothing -> (a * c) + sumCached (M.insert a c m) as l
+    Just v -> (a * v) + sumCached m as l
+  where
+    c = length $ filter (== a) l
+
+main :: IO ()
+main = do
+    [fileLoc] <- getArgs
+    file <- readFile fileLoc
+
+    -- Part 1
+    let (a, b) = convert $ filter (not . null) $ lines file
+        (a', b') = (sort a, sort b)
+        l = zipWith ((abs .) . (-)) a' b'
+    print (sum l)
+
+    -- Part 2
+    print (sumCached M.empty a' b')
