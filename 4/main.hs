2034f9a7eb6df35c85e3875ec810715572a93511
Thomas Lohse <thomas.k.lohse@gmail.com>
2024-12-04 08:08:50 +0100

    day 4

--- 4/main.hs
+++ 4/main.hs
@@ -1,45 +1,50 @@
 import Data.List
 import System.Environment
 
-findXmas :: [String] -> (Int, Int) -> Int
-findXmas content i = length $ filter (findSequence content "XMAS" i) dirs
+data Direction = North | NorthEast | East | SouthEast | South | SouthWest | West | NorthWest
+
+type Grid a = [[a]]
+type Loc = (Int, Int) -- (LINE, CHAR)
+
+step :: Loc -> Direction -> Loc
+step (l, c) North = (l - 1, c)
+step (l, c) NorthEast = (l - 1, c + 1)
+step (l, c) East = (l, c + 1)
+step (l, c) SouthEast = (l + 1, c + 1)
+step (l, c) South = (l + 1, c)
+step (l, c) SouthWest = (l + 1, c - 1)
+step (l, c) West = (l, c - 1)
+step (l, c) NorthWest = (l - 1, c - 1)
+
+findXmas :: [String] -> Loc -> Int
+findXmas content i = length $ filter (\d -> findSequence content "MAS" (step i d) d) dirs
   where
     dirs = [North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest]
 
-data Direction = North | NorthEast | East | SouthEast | South | SouthWest | West | NorthWest
-
-findSequence :: (Eq a) => [[a]] -> [a] -> (Int, Int) -> Direction -> Bool
+findSequence :: (Eq a) => Grid a -> [a] -> Loc -> Direction -> Bool
 findSequence _ [] _ _ = True
-findSequence content (s : search) (l, c) direction
-    | l >= 0 && l < length content && c >= 0 && c < length (content !! l) && content !! l !! c == s = findSequence content search newIndex direction
-    | otherwise = False
+findSequence content (s : search) (l, c) dir = bounds && content !! l !! c == s && findSequence content search (step (l, c) dir) dir
   where
-    newIndex = case direction of
-        North -> (l - 1, c)
-        NorthEast -> (l - 1, c + 1)
-        East -> (l, c + 1)
-        SouthEast -> (l + 1, c + 1)
-        South -> (l + 1, c)
-        SouthWest -> (l + 1, c - 1)
-        West -> (l, c - 1)
-        NorthWest -> (l - 1, c - 1)
+    bounds = l >= 0 && l < length content && c >= 0 && c < length (content !! l)
 
-findX :: [String] -> (Int, Int) -> Bool
-findX content i =
-    (findLetters "MS" || findLetters "SM")
-        && (findLetters' "MS" || findLetters' "SM")
+findX :: [String] -> Loc -> Bool
+findX content i = any downSearch s && any upSearch s
   where
-    findLetters [a, b] = findSequence content ['A', a] i NorthWest && findSequence content ['A', b] i SouthEast
-    findLetters' [a, b] = findSequence content ['A', a] i NorthEast && findSequence content ['A', b] i SouthWest
+    s = ["MAS", "SAM"]
+    downSearch s = findSequence content s (step i NorthWest) SouthEast
+    upSearch s = findSequence content s (step i NorthEast) SouthWest
+
+-- \|| findSequence content s (step i SouthEast) NorthWest
 
 main = do
     [fileLoc] <- getArgs
     file <- lines <$> readFile fileLoc
+    let findEl s f = concatMap (\(a, b) -> map (a,) b) $ zipWith (\a b -> (a, elemIndices s b)) [0 ..] f
     -- Part 1
-    let xs :: [(Int, Int)] -- (LINE, CHAR)
-        xs = concatMap (\(a, b) -> map (a,) b) $ zipWith (\a b -> (a, elemIndices 'X' b)) [0 ..] file
+    let xs :: [Loc]
+        xs = findEl 'X' file
     print $ sum $ map (findXmas file) xs
     -- Part 2
-    let xs' :: [(Int, Int)] -- (LINE, CHAR)
-        xs' = concatMap (\(a, b) -> map (a,) b) $ zipWith (\a b -> (a, elemIndices 'A' b)) [0 ..] file
+    let xs' :: [Loc]
+        xs' = findEl 'A' file
     print $ length $ filter (findX file) xs'
